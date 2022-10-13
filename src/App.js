403ee6640064fd685234479a4b172aea7c79b606
andreszapataq <andreszapataq@gmail.com>
2022-10-12 23:08:34 -0500

    Âadd SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./layout/Header";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,9 @@
+const SearchBar = () => {
+  return (
+    <form>
+        <input type="text" placeholder="Buscar bodega" />
+    </form>
+  )
+}
+
+export default SearchBar
