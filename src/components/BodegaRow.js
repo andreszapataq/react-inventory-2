7459535b341438ad7be000bf50790eab63ab3f99
andreszapataq <andreszapataq@gmail.com>
2022-11-10 08:27:13 -0500

    test deploy

--- src/components/BodegaRow.js
+++ src/components/BodegaRow.js
@@ -1,7 +1,7 @@
 const BodegaRow = () => {
   return (
     <div className="w-1/2">
-        <h2 className="font-semibold text-lg">Principal</h2>
+        <h2 className="font-semibold text-lg">Principa</h2>
         <p className="font-light text-xs">Vive Solutions - 900274934</p>
     </div>
   )
