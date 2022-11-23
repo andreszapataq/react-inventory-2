d3fcd7580616c3cc6ecd77eea19e7fc546a6d487
andreszapataq <andreszapataq@gmail.com>
2022-11-22 19:11:05 -0500

    add conditional rendering to Header component

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,8 +1,21 @@
+import { useLocation } from "react-router-dom"
+
 const Header = () => {
+  let location = useLocation()
+  
   return (
-    <div className="w-4/5 lg:w-1/2 flex justify-center">
-        <h1 className="text-3xl font-semibold py-12">Bodegas</h1>
-        <p></p>
+    <div className="w-4/5 lg:w-1/2 flex justify-center py-12">
+      {location.pathname === "/" && (
+        <div>
+          <h1 className="text-3xl font-semibold">Bodegas</h1>
+        </div>
+      )}
+      {location.pathname.includes('/bodega/') && (
+        <div className="flex flex-col items-center">
+          <h1 className="text-3xl font-semibold">Hospital</h1>
+          <p className="text-xl">Bodega</p>
+        </div>
+      )}
     </div>
   )
 }
--- src/pages/Bodega.js
+++ src/pages/Bodega.js
@@ -1,13 +1,9 @@
-import { useParams } from "react-router-dom"
 import ItemContainer from "../layout/ItemContainer"
 import ItemRow from "../components/ItemRow"
 
 function Bodega() {
-  const params = useParams()
-  
   return (
     <ItemContainer>
-      Bodega {params.id}
       <ItemRow />
     </ItemContainer>
   )
