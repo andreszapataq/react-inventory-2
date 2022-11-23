d220fb2e7909dfcae62bd447eeb5e2765d8f8630
andreszapataq <andreszapataq@gmail.com>
2022-11-22 19:49:47 -0500

    fix layout with grid

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,14 +4,17 @@ const Header = () => {
   let location = useLocation()
   
   return (
-    <div className="w-4/5 lg:w-1/2 flex justify-center py-12">
+    <div className="w-4/5 lg:w-1/2 flex flex-col items-center">
       {location.pathname === "/" && (
-        <div>
+        <div className="grid grid-rows-3 py-8">
+          <p></p>
           <h1 className="text-3xl font-semibold">Bodegas</h1>
+          <p></p>
         </div>
       )}
       {location.pathname.includes('/bodega/') && (
-        <div className="flex flex-col items-center">
+        <div className="grid grid-rows-3 py-8 justify-items-center">
+          <p></p>
           <h1 className="text-3xl font-semibold">Hospital</h1>
           <p className="text-xl">Bodega</p>
         </div>
