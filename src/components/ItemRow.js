d1aee54d69546e7a8afc8680226221a192544277
andreszapataq <andreszapataq@gmail.com>
2022-11-27 17:57:50 -0500

    update ItemRow

--- src/components/ItemRow.js
+++ src/components/ItemRow.js
@@ -11,9 +11,12 @@ const ItemRow = ({referencia, lote}) => {
             <h2 className="text-lg font-semibold">{referencia.nombre}</h2>
             <p className="text-xs font-light">{referencia.codigo}</p>
           </div>
-          <div className="flex flex-col items-center border-solid border-l border-light-gray pl-8">
-            <p className="text-xl font-semibold">{referencia.cantidad}</p>
-            <p className="text-xs font-light">Unidades</p>
+          <div className="flex justify-end items-center">
+            <div className="h-12 w-px bg-light-gray mr-8"></div>
+            <div className="flex flex-col items-center w-16">
+              <p className="text-xl font-semibold">{referencia.cantidad}</p>
+              <p className="text-xs font-light">Unidades</p>
+            </div>
           </div>
         </div>
       )}
@@ -23,9 +26,12 @@ const ItemRow = ({referencia, lote}) => {
             <h2 className="text-lg font-semibold">{lote.lote}</h2>
             <p></p>
           </div>
-          <div className="flex flex-col items-center border-solid border-l border-light-gray pl-8">
-            <p className="text-xs font-light">{lote.fecha_vencimiento}</p>
-            <p></p>
+          <div className="flex justify-end items-center">
+            <div className="h-12 w-px bg-light-gray mr-8"></div>
+            <div className="flex flex-col items-center w-16">
+              <p className="text-xs font-light">{lote.fecha_vencimiento}</p>
+              <p></p>
+            </div>
           </div>
         </div>
       )}
