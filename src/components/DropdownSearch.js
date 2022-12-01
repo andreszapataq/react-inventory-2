0d7773d3fbe9b738218cbc3a609e2ae94bd463d3
andreszapataq <andreszapataq@gmail.com>
2022-12-01 11:01:06 -0500

    add grid to SecondaryLayout

--- src/components/DropdownSearch.js
+++ src/components/DropdownSearch.js
@@ -1,7 +1,15 @@
+import {FaCaretSquareDown} from 'react-icons/fa'
+
 const DropdownSearch = () => {
   return (
     <div>
-        Dropdown
+      <div className='flex justify-between items-center w-[287px] h-[38px] px-2 py-1 border border-cadet-blue-crayola rounded-lg'>
+        <div>
+          <p className='text-xs'>Hospital San José</p>
+          <p className='text-[9px] font-light'>Megatecnología y Rehabilitación Colombiana - 900094992</p>
+        </div>
+        <FaCaretSquareDown className='fill-cool-grey' />
+      </div>
     </div>
   )
 }
--- src/layout/SecondaryLayout.js
+++ src/layout/SecondaryLayout.js
@@ -3,25 +3,31 @@ import LoteLog from "../components/LoteLog"
 
 const SecondaryLayout = () => {
   return (
-    <div>
+    <div className="p-8">
       <div>
       <h1 className="text-3xl font-semibold">Lote No.</h1>
       <p className="text-xl">Fecha de Vencimiento</p>
+      <div className="w-full h-px bg-cadet-blue-crayola mr-8"></div>
       </div>
-      <div>
-        <p>Bodega actual</p>
-      </div>
-      <div>
-        <p>Trasladar a</p>
-        <DropdownSearch />
-      </div>
-      <div>
-        <p>Notas</p>
-        <textarea name="notes" id="notes" className="resize-none w-[461px] h-[104px] px-2 py-1 border border-cadet-blue-crayola rounded-lg"></textarea>
-      </div>
-      <div>
-        <p>Registro</p>
-        <LoteLog />
+      <div className="grid grid-rows-5 gap-11">
+        <div>
+          <p>Bodega actual</p>
+        </div>
+        <div>
+          <p>Trasladar a</p>
+          <DropdownSearch />
+        </div>
+        <div>
+          <p>Notas</p>
+          <textarea name="notes" id="notes" className="resize-none w-[461px] h-[104px] px-2 py-1 border border-cadet-blue-crayola rounded-lg"></textarea>
+        </div>
+        <div>
+          <p>Registro</p>
+          <LoteLog />
+        </div>
+        <div>
+          Buttons
+        </div>
       </div>
     </div>
   )
