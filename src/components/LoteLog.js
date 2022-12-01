467df8113ce9145cae651fae9c343fad99799fb4
andreszapataq <andreszapataq@gmail.com>
2022-11-30 23:18:17 -0500

    update LoteLog and SecondaryLayout

--- src/components/LoteLog.js
+++ src/components/LoteLog.js
@@ -1,6 +1,8 @@
 const LoteLog = () => {
   return (
-    <textarea name="log" id="log" className="resize-none w-[461px] h-[246px] border border-cadet-blue-crayola rounded-lg"></textarea>
+    <div className="w-[461px] h-[246px] border border-cadet-blue-crayola rounded-lg">
+      
+    </div>
   )
 }
 
--- src/layout/SecondaryLayout.js
+++ src/layout/SecondaryLayout.js
@@ -5,6 +5,10 @@ const SecondaryLayout = () => {
   return (
     <div>
       <div>
+      <h1 className="text-3xl font-semibold">Lote No.</h1>
+      <p className="text-xl">Fecha de Vencimiento</p>
+      </div>
+      <div>
         <p>Bodega actual</p>
       </div>
       <div>
@@ -13,7 +17,7 @@ const SecondaryLayout = () => {
       </div>
       <div>
         <p>Notas</p>
-        <textarea name="notes" id="notes" className="resize-none w-[461px] h-[104px] border border-cadet-blue-crayola rounded-lg"></textarea>
+        <textarea name="notes" id="notes" className="resize-none w-[461px] h-[104px] px-2 py-1 border border-cadet-blue-crayola rounded-lg"></textarea>
       </div>
       <div>
         <p>Registro</p>
