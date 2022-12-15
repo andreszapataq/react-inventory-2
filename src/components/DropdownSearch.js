8b303bd9ee864e81dfcf12a1a690c566eb6d54d0
andreszapataq <andreszapataq@gmail.com>
2022-12-14 22:07:50 -0500

    add filter function to the DropdownSearch component

--- src/components/DropdownSearch.js
+++ src/components/DropdownSearch.js
@@ -4,7 +4,18 @@ import DropdownRow from './DropdownRow'
 
 const DropdownSearch = ({bodegas}) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [filteredBodegas, setFilteredBodegas] = useState(bodegas)
   const wrapperRef = useRef(null)
+
+  const handleFilter = event => {
+    const searchTerm = event.target.value.toLowerCase()
+    const filtered = bodegas.filter(bodega =>
+      Object.values(bodega).some(value =>
+        typeof value === 'string' && value.toLowerCase().includes(searchTerm)
+      )  
+    )
+    setFilteredBodegas(filtered)
+  }
 
   useEffect(() => {
     function handleClickOutside(event) {
@@ -43,12 +54,12 @@ const DropdownSearch = ({bodegas}) => {
           <div className="py-1 rounded-lg bg-white">
             <div className='flex items-center px-3 py-2 bg-ghost-white rounded-tl-lg rounded-tr-lg'>
               <FaSearch size={14} className='fill-cool-grey mr-2' />
-              <input type="text" className="w-full text-slate-500 text-xs bg-ghost-white placeholder:text-cool-grey focus:outline-none" placeholder="Buscar bodega" />
+              <input type="text" onChange={handleFilter} className="w-full text-slate-500 text-xs bg-ghost-white placeholder:text-cool-grey focus:outline-none" placeholder="Buscar bodega" />
             </div>
             <div className="border-t border-gray-100" />
             <div className='px-4 py-2 text-[10px] text-cool-grey'>Sugerencias</div>
             <div>
-              {bodegas.map((bodega, index) => (
+              {filteredBodegas.map((bodega, index) => (
                 <DropdownRow key={index} bodega={bodega} />
               ))}
             </div>
