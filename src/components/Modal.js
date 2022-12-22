6eea9b9a897811d73163b7af417d22c5f8e1e387
andreszapataq <andreszapataq@gmail.com>
2022-12-21 21:17:41 -0500

    update Modal component, add all elements

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,14 +1,29 @@
 import { FaTimesCircle } from "react-icons/fa"
 
+import DropdownSearch from "./DropdownSearch"
+import Button from "./Button"
+
 const Modal = ({toggleModal}) => {
   return (
     <div className="modal">
-        <div className="w-[412px] h-[452px] bg-white p-5">
-            <div className="flex justify-end p-2">
-                <FaTimesCircle className="cursor-pointer" onClick={toggleModal} />
-            </div>
-            <h2 className="text-xl font-semibold">Crear bodega</h2>
+      <div className="flex flex-col gap-5 w-[412px] h-[452px] bg-white p-5">
+        <div className="flex justify-end">
+            <FaTimesCircle className="cursor-pointer" onClick={toggleModal} />
         </div>
+        <h2 className="text-2xl font-semibold">Crear bodega</h2>
+        <div>
+          <h3 className="text-lg font-semibold">Nombre</h3>
+          <input type="text" className="border" />
+        </div>
+        <div>
+          <h3 className="text-lg font-semibold">Cliente</h3>
+          <DropdownSearch />
+        </div>
+        <div>
+          <Button variant="cancel" label="Cancelar" />
+          <Button variant="confirm" label="Crear bodega" />
+        </div>
+      </div>
     </div>
   )
 }
