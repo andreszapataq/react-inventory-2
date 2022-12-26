66e7d94f2913e3ca7aea4fb47a103775402f1771
andreszapataq <andreszapataq@gmail.com>
2022-12-26 16:21:56 -0500

    update Modal spacing

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,11 +6,11 @@ import Button from "./Button"
 const Modal = ({toggleModal, clientes}) => {
   return (
     <div className="modal">
-      <div className="flex flex-col gap-7 w-[412px] h-[452px] border-2 border-black bg-white p-5 rounded-lg">
+      <div className="flex flex-col gap-6 w-[412px] h-[492px] border-2 border-black bg-white p-5 rounded-lg">
         <div className="flex justify-end">
             <FaTimesCircle className="cursor-pointer" onClick={toggleModal} />
         </div>
-        <h2 className="text-2xl font-semibold">Crear bodega</h2>
+        <h2 className="text-2xl font-semibold -mt-3">Crear bodega</h2>
         <div>
           <h3 className="text-lg font-semibold mb-2">Nombre</h3>
           <input type="text" className="w-[346px] h-[38px] px-2 py-1 border border-cadet-blue-crayola rounded-lg" />
@@ -19,7 +19,7 @@ const Modal = ({toggleModal, clientes}) => {
           <h3 className="text-lg font-semibold mb-2">Cliente</h3>
           <DropdownSearch label="Seleccionar cliente" clientes={clientes} />
         </div>
-        <div className="w-[346px] flex justify-end gap-4 mt-10">
+        <div className="w-[346px] flex justify-end gap-4 mt-24">
           <Button variant="cancel" label="Cancelar" />
           <Button variant="confirm" label="Crear bodega" />
         </div>
