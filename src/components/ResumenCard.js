bb379a56795e6dcca911fa22780a173701284b7a
andreszapataq <andreszapataq@gmail.com>
2023-01-01 01:25:20 -0500

    update ResumenCard component

--- src/components/ResumenCard.js
+++ src/components/ResumenCard.js
@@ -1,6 +1,8 @@
 const ResumenCard = () => {
   return (
-    <div>ResumenCard</div>
+    <div className="w-[428px] h-[569px] border rounded-lg shadow-lg">
+      <h1 className="text-xl font-semibold p-4">Resumen</h1>
+    </div>
   )
 }
 
