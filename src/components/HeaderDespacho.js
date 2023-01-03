80a97d4baa796f0903d41b5de914d3f86bd9e905
andreszapataq <andreszapataq@gmail.com>
2023-01-02 21:58:01 -0500

    HeaderDespacho font sizes

--- src/components/HeaderDespacho.js
+++ src/components/HeaderDespacho.js
@@ -3,9 +3,9 @@ import { MdOutlineSwapHorizontalCircle } from 'react-icons/md'
 const HeaderDespacho = () => {
   return (
     <div className="flex justify-between items-center">
-      <div>Desde</div>
+      <div className='font-light text-xs'>Desde</div>
       <MdOutlineSwapHorizontalCircle size={33} className="fill-raisin-black cursor-pointer" />
-      <div>Hacia</div>
+      <div className='font-light text-xs'>Hacia</div>
     </div>
   )
 }
