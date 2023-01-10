d610069750db943197319ae317d2c39105e468cc
andreszapataq <andreszapataq@gmail.com>
2023-01-09 22:35:22 -0500

    add DespachoRow new width

--- src/components/DespachoRow.js
+++ src/components/DespachoRow.js
@@ -1,6 +1,6 @@
 const DespachoRow = () => {
   return (
-    <div className="flex justify-between items-center bg-cultured rounded-lg px-8 py-4 mb-4">
+    <div className="flex justify-between items-center bg-cultured rounded-lg lg:w-1/2 px-8 py-4 mb-4">
       <div>
         <h2 className="text-lg font-semibold">MORA210189-054</h2>
         <p className="text-xs font-light">DBM Putty 5.00 CC - AT679FD</p>
