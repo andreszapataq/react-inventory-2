14ca3794b248152fe11db524425ae2a42034350a
andreszapataq <andreszapataq@gmail.com>
2023-01-17 13:43:40 -0500

    done inventarios with lotes

--- src/data.js
+++ src/data.js
@@ -35,22 +35,68 @@ export const INVENTARIO_PRINCIPAL = [
     {
         codigo: 'AT677FD',
         nombre: 'DBM Putty 1.00 CC',
-        cantidad: 5
+        cantidad: 5,
+        lotes: [
+            'LOPA21101474-136',
+            'LOPA21101474-077',
+            'LOPA21101474-133',
+            'LOPA21101474-132',
+            'LOPA21101474-121'
+        ]
     },
     {
         codigo: 'AT678FD',
         nombre: 'DBM Putty 2.50 CC',
-        cantidad: 7
+        cantidad: 7,
+        lotes: [
+            'SDS200411-072',
+            'LET21014568-134',
+            'LET21014568-091',
+            'SDS200411-055',
+            'LET21014568-104',
+            'LET21014568-141',
+            'LET21014568-082'
+        ]
     },
     {
         codigo: 'AT679FD',
         nombre: 'DBM Putty 5.00 CC',
-        cantidad: 11
+        cantidad: 11,
+        lotes: [
+            'MSTF0915203-116',
+            'LOPA21070363-044',
+            'ATSFWI2200020-046',
+            'MSTF0915203-103',
+            'MSTF0915203-118',
+            'MORA210280-120',
+            'MORA210280-118',
+            'LOPA21110370-062',
+            'ATSFWI221147-041',
+            'MSTF0915203-115',
+            'MORA210280-111'
+        ]
     },
     {
         codigo: 'AT805FD',
         nombre: 'Cancellous Crushed/Chips 10 CC (4-10mm) FD',
-        cantidad: 15
+        cantidad: 15,
+        lotes: [
+            'LSDR210600397-010',
+            'LET20013378-010',
+            'LOPA21070995-029',
+            'LSDR210800295-014',
+            'LET22014690-012',
+            'LOPA21070995-030',
+            'ATSFWI221053-016',
+            'LOPA21092434-012',
+            'LOPA21091414-031',
+            'LET21014431-002',
+            'LET21014445-018',
+            'LSDR210800295-016',
+            'LSDR210800295-015',
+            'LSDR210800374-004',
+            'LSDR210800374-008'
+        ]
     }
 ]
 
@@ -58,17 +104,30 @@ export const INVENTARIO_SAN_JOSE_SAN_JOSE = [
     {
         codigo: 'AT677FD',
         nombre: 'DBM Putty 1.00 CC',
-        cantidad: 3
+        cantidad: 3,
+        lotes: [
+            'MORA200211-126',
+            'LOPA21101474-082',
+            'LOPA21101474-125'
+        ]
     },
     {
         codigo: 'AT807FD',
         nombre: 'Cancellous Crushed/Chips 15 CC (4-10mm) FD',
-        cantidad: 3
+        cantidad: 3,
+        lotes: [
+            'LOPA21121152-019',
+            'LOPA21121152-021',
+            'LOPA21121152-023'
+        ]
     },
     {
         codigo: 'AT302FD',
         nombre: 'Tricortical Blk 16-19 MM FD',
-        cantidad: 1
+        cantidad: 1,
+        lotes: [
+            'MORA220013-056'
+        ]
     }
 ]
 
@@ -76,12 +135,21 @@ export const INVENTARIO_SAN_JOSE_MEGATECNOLOGIA = [
     {
         codigo: 'AT680FD',
         nombre: 'DBM Putty 10.0 CC',
-        cantidad: 4
+        cantidad: 4,
+        lotes: [
+            'LSDR211000351-093',
+            'LOPA22010231-151',
+            'LSDR211000351-070',
+            'LSDR211200830-046'
+        ]
     },
     {
         codigo: 'AT447FD',
         nombre: 'Femoral Head',
-        cantidad: 1
+        cantidad: 1,
+        lotes: [
+            'SDS181008-007'
+        ]
     }
 ]
 
