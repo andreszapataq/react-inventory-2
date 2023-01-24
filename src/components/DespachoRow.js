740207343d097bfedaeb9c1e50b2745f02fd0c1d
andreszapataq <andreszapataq@gmail.com>
2023-01-24 18:55:50 -0500

    DespachoRow in Despacho map data

--- src/components/DespachoRow.js
+++ src/components/DespachoRow.js
@@ -1,7 +1,9 @@
 import { useState } from "react"
 import { FaCheckCircle } from "react-icons/fa"
 
-const DespachoRow = () => {
+const DespachoRow = ({item}) => {
+  console.log(item)
+
   const [check, setCheck] = useState(false)
 
   const handleCheck = () => {
@@ -11,12 +13,12 @@ const DespachoRow = () => {
   return (
     <div className="w-3/5 flex justify-between items-center bg-cultured rounded-lg pl-8 pr-4 py-4 mb-4 cursor-pointer" onClick={handleCheck}>
       <div>
-        <h2 className="text-lg font-semibold">MORA210189-054</h2>
-        <p className="text-xs font-light">DBM Putty 5.00 CC - AT679FD</p>
+        <h2 className="text-lg font-semibold">{item.lote}</h2>
+        <p className="text-xs font-light">{item.nombre} - {item.codigo}</p>
       </div>
       <div className="flex justify-end items-center">
         <div className="h-12 w-px bg-green-300 mr-8"></div>
-        <p className="text-xs font-light">23/03/2025</p>
+        <p className="text-xs font-light">{item.fecha_vencimiento}</p>
         {check ? <FaCheckCircle size={13} className="self-start fill-raisin-black" /> : <div className="w-[13px] h-[13px] self-start rounded-full border border-cool-grey"></div>}
       </div>
     </div>
--- src/pages/Despacho.js
+++ src/pages/Despacho.js
@@ -8,9 +8,22 @@ import DespachoRow from "../components/DespachoRow"
 function Despacho() {
   let location = useLocation()
 
-  const {nombre_bodega = "", cliente = "", id_number = ""} = location.state?.bodega || {}
+  const {nombre_bodega = "", cliente = "", id_number = "", stock = ""} = location.state?.bodega || {}
 
   console.log(location.state?.bodega)
+
+  const newStock = stock.map(item => {
+    return item.lotes.map(lote => {
+      return {
+        codigo: item.codigo,
+        nombre: item.nombre,
+        lote: lote.lote,
+        fecha_vencimiento: lote.fecha_vencimiento
+      }
+    })
+  }).flat()
+
+  console.log(newStock)
 
   return (
     <div className="flex flex-col gap-11 p-10">
@@ -19,7 +32,9 @@ function Despacho() {
       <div className="flex justify-between">
         <div className="w-2/3 flex flex-col">
           <SearchBar />
-          <DespachoRow />
+          {newStock.map((item, index) => (
+            <DespachoRow key={index} item={item} />
+          ))}
         </div>
         <div className="w-1/3 flex flex-col items-end">
           <ResumenCard />
