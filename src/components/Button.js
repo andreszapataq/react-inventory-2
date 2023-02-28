4a1a92974b3df8243758d66bcf2f8bd10fadc6d4
andreszapataq <andreszapataq@gmail.com>
2023-02-27 23:35:15 -0500

    clg for nothing, again!

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,6 @@
 const Button = ({variant, label, onClick}) => {
+  console.log(label)
+
   return (
     <button className={`w-[149px] h-[42px] rounded-[21px] font-medium btn-${variant}`} onClick={onClick}>
       {label}
