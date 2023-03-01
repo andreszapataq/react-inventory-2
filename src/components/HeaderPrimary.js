923dd1c45d4f989b5c95be13b0d43d53feac07bd
andreszapataq <andreszapataq@gmail.com>
2023-02-28 21:23:11 -0500

    clg clientes in HeaderPrimary

--- src/components/HeaderPrimary.js
+++ src/components/HeaderPrimary.js
@@ -5,6 +5,7 @@ import {FaWarehouse} from 'react-icons/fa'
 import Modal from "./Modal"
 
 const HeaderPrimary = ({clientes}) => {
+  console.log(clientes)
   let { id } = useParams()
   let location = useLocation()
 
