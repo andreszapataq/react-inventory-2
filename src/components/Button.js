da92c435d9353cbb7daebae099486172d68c144a
andreszapataq <andreszapataq@gmail.com>
2023-03-01 22:30:41 -0500

    delete clg in Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,4 @@
 const Button = ({variant, label, onClick}) => {
-  console.log(label)
-
   return (
     <button className={`w-[149px] h-[42px] rounded-[21px] font-medium btn-${variant}`} onClick={onClick}>
       {label}
